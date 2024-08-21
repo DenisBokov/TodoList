80a293c1ab1081eb850a4a75bed7d4b5e6dc4bdb
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-21 16:36:13 +0300

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Denis Bokov on 21.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
